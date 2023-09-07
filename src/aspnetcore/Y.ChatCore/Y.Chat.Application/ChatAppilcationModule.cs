f082b2b66d546db5306174ef0ae699d64ea107c7
YG-Calo <3164522206@qq.com>
2023-09-07 22:26:27 +0800

    初始化后端模块以及前端·代码

---
+++ src/aspnetcore/Y.ChatCore/Y.Chat.Application/ChatAppilcationModule.cs
@@ -0,0 +1,15 @@
+﻿using Y.Chat.EntityCore;
+using Y.Module;
+using Y.Module.Modules;
+
+namespace Y.Chat.Application
+{
+    [DependOn(typeof(ChatEntityCoreModule))]
+    public class ChatAppilcationModule:YModule
+    {
+        public override void ConfigerService(ConfigerServiceContext context)
+        {
+            base.ConfigerService(context);
+        }
+    }
+}
---
+++ src/aspnetcore/Y.ChatCore/Y.Chat.Host/ChatHostModule.cs
@@ -0,0 +1,22 @@
+﻿using Y.Module;
+using Y.Module.Extensions;
+using Y.Module.Modules;
+
+namespace Y.Chat.Host
+{
+    public class ChatHostModule:YModule
+    {
+        public override void ConfigerService(ConfigerServiceContext context)
+        {
+            // Add services to the container.
+            context.Services.AddControllersWithViews();
+        }
+
+        public override void InitApplication(InitApplicationContext context)
+        {
+            var app = context.GetApplicationBuilder();
+
+            var env = (IHostEnvironment)context.ServiceProvider.GetRequiredService<IWebHostEnvironment>();
+        }
+    }
+}

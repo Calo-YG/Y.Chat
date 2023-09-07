fea5b9c424bb639660e3aa56f42de22a09b5a332
YG-Calo <3164522206@qq.com>
2023-09-07 22:42:49 +0800

    小修改

--- src/web/Y.Chat.Web/src/main.ts
+++ src/web/Y.Chat.Web/src/main.ts
@@ -4,13 +4,20 @@ import {createPinia} from 'pinia'
 import 'element-plus/dist/index.css'
 import './style.css'
 import App from './App.vue'
+//import zhCn from 'element-plus/dist/locale/zhCn.mjs'
+
+
 
 const store = createPinia()
 
 const app = createApp(App)
 
 app.use(store)
-  
+
+// app.use(ElementPlus, {
+//     locale: zhCn,
+//   })
+
 app.use(ElementPlus)
 
 app.mount('#app')

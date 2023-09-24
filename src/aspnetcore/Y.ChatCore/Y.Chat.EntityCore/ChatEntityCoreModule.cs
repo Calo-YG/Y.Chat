07387ab03461fdd3aba471354122e07653bab86e
YG-Calo <3164522206@qq.com>
2023-09-25 00:59:46 +0800

    初步搭建基于masa framework 后端

--- src/aspnetcore/Y.ChatCore/Y.Chat.EntityCore/ChatEntityCoreModule.cs
+++ src/aspnetcore/Y.ChatCore/Y.Chat.EntityCore/ChatEntityCoreModule.cs
@@ -1,20 +1,34 @@
 ﻿using Calo.Blog.Common;
 using Calo.Blog.Common.Minio;
 using Calo.Blog.Common.Redis;
+using Masa.BuildingBlocks.Caching;
+using Masa.BuildingBlocks.Ddd.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Y.Module;
 using Y.Module.Modules;
 
 namespace Y.Chat.EntityCore
 {
-    [DependOn(typeof(CommonModule)
-        ,typeof(MinioModule)
-        ,typeof(RedisModule))]
-    public class ChatEntityCoreModule:YModule
+    [DependOn(typeof(CommonModule), typeof(MinioModule), typeof(RedisModule))]
+    public class ChatEntityCoreModule : YModule
     {
         public override void ConfigerService(ConfigerServiceContext context)
         {
+            context.Services.AddMasaDbContext<YChatContext>(optionsBuilder =>
+            {
+                optionsBuilder.UseSqlServer();
+                optionsBuilder.UseFilter();
+            });
 
-            base.ConfigerService(context);
+            context.Services.AddDomainEventBus(options =>
+            {
+                options.UseRepository<YChatContext>();
+            });
+
+            context.Services.AddMultilevelCache(
+                cacheBuilder => cacheBuilder.UseStackExchangeRedisCache()
+            );
         }
     }
 }
---
+++ src/aspnetcore/Y.ChatCore/Y.Chat.EntityCore/YChatContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Y.Chat.EntityCore
+{
+    public class YChatContext: MasaDbContext<YChatContext>
+    {
+        public YChatContext(MasaDbContextOptions<YChatContext> masaDbContextOptions) : base(masaDbContextOptions)
+        {
+        }
+    }
+}

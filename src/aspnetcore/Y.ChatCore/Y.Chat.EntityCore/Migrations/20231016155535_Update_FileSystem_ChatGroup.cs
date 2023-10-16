9f14d58ad8ae7e13a0e49b0dd5cc364c2315bf43
YG-Calo <3164522206@qq.com>
2023-10-16 23:56:47 +0800

    修改codefirst文件

---
+++ src/aspnetcore/Y.ChatCore/Y.Chat.EntityCore/Migrations/20231016155535_Update_FileSystem_ChatGroup.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Y.Chat.EntityCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update_FileSystem_ChatGroup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "Bachelors",
+                table: "ChatGroups",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<string>(
+                name: "GroupNumber",
+                table: "ChatGroups",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Bachelors",
+                table: "ChatGroups");
+
+            migrationBuilder.DropColumn(
+                name: "GroupNumber",
+                table: "ChatGroups");
+        }
+    }
+}
